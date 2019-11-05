cfc89fd85553e917d396a4d4cacd0ea485c4cb78
msjensen <hr.msjensen@gmail.com>
2019-11-05 21:54:24 +0100

    add files via upload

--- src/AD8495.h
+++ src/AD8495.h
@@ -1,27 +1,20 @@
 #ifndef AD8495_H
 #define AD8495_H
 
-#include "Arduino.h"
+//#include "Arduino.h"
 
 class AD8495 {
   public:
-    int _aref = 5;
-    int _adcResolution = 10;
-    int _analogPin;
-    int _adc_max_value = pow(2,_adcResolution)-1;
-    double _voltage = 0;
 
     AD8495(int analogPin);
     double readCelsius(void);
 
   private:
-    /*
     int _aref = 5;
     int _adcResolution = 10;
     int _analogPin;
-    int _adc_max_value = pow(2,_adcResolution);
+    int _adc_max_value = pow(2,_adcResolution)-1;
     double _voltage = 0;
-    */
 };
 
 #endif
