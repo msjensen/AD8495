ef4258e879c9bed2f820037ade0e77c026eb5e64
msjensen <hr.msjensen@gmail.com>
2019-11-06 21:49:06 +0100

    small changes

--- src/AD8495.cpp
+++ src/AD8495.cpp
@@ -1,13 +1,31 @@
-#include "AD8495.h"
+#include <AD8495.h>
 #include "Arduino.h"
 
 //double adc_voltage_resolution = AREF/adc_max_value;
 
 AD8495::AD8495(int analogPin) {
   _analogPin = analogPin;
+  _adc_max_value = pow(2,_adcResolution)-1;
+}
+
+AD8495::AD8495(int analogPin, double adcRefV) {
+  _analogPin = analogPin;
+  _adcRefV = adcRefV;
+  _adc_max_value = pow(2,_adcResolution)-1;
+}
+
+AD8495::AD8495(int analogPin, int adcRefV, int adcRes) {
+  _analogPin = analogPin;
+  _adcRefV = adcRefV;
+  _adcResolution = adcRes;
+  _adc_max_value = pow(2,_adcResolution)-1;
 }
 
 double AD8495::readCelsius(void) {
-  _voltage = (analogRead(_analogPin)*_aref) / _adc_max_value;
-  return (_voltage - 2.49) / 0.005;
+  _voltage = (analogRead(_analogPin)*_adcRefV) / _adc_max_value;
+  return (_voltage - _ampRefV) / 0.005;
+}
+
+void AD8495::setAmpRefV(double ampRefV) {
+  _ampRefV = ampRefV;
 }
--- src/AD8495.h
+++ src/AD8495.h
@@ -1,20 +1,21 @@
 #ifndef AD8495_H
 #define AD8495_H
 
-//#include "Arduino.h"
-
 class AD8495 {
   public:
 
     AD8495(int analogPin);
+    AD8495(int analogPin, double adcRefV);
+    AD8495(int analogPin, int adcRefV, int adcRes);
     double readCelsius(void);
+    void setAmpRefV(double ampRefV);
 
   private:
-    int _aref = 5;
-    int _adcResolution = 10;
     int _analogPin;
-    int _adc_max_value = pow(2,_adcResolution)-1;
+    int _adc_max_value;
+    int _adcResolution = 10;
+    double _adcRefV = 5;
+    double _ampRefV = 1.842;
     double _voltage = 0;
 };
-
 #endif
